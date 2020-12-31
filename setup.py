b3e2a7ff84d13b5546e97929200f6a010e435a06
AbdelnasserMustafa <nasser02@yahoo.com>
2020-12-31 01:26:00 -0800

    added notbooks and python files

---
+++ setup.py
@@ -0,0 +1,16 @@
+# setup.py
+
+import setuptools
+setuptools.setup(
+    name = "nasser-m-package",
+    version = "1.0.0",
+    author = "Nasser M Abdelghani",
+    author_email = "nasser02@email.com",
+    description = "packt example package",
+    long_description = " THis is the longer description and will appear in the web",
+    py_modules = ["packt"],
+    classifiers = [
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+)
